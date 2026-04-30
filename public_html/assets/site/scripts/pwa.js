a81232b07fd3a2fa5da39c0834bcc4fe95751d99
Arian Ghasempour <arianghsm721@gmail.com>
2026-04-30 10:10:48 +0330

    chore: sync deployed laptop state to github

--- public_html/assets/site/scripts/pwa.js
+++ public_html/assets/site/scripts/pwa.js
@@ -3,7 +3,7 @@
         return;
     }
 
-    var CURRENT_VERSION = "20260430-100743";
+    var CURRENT_VERSION = "20260430-101021";
     var VERSION_ENDPOINT = "/app-version.json";
     var SERVICE_WORKER_ENDPOINT = "/sw.js";
     var UPDATE_CHECK_MIN_INTERVAL = 5000;
--- public_html/sw.js
+++ public_html/sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "20260430-100743";
+const APP_VERSION = "20260430-101021";
 const STATIC_CACHE = "dent1402-static-" + APP_VERSION;
 
 const STATIC_ASSETS = [
