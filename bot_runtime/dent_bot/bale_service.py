bf161927ace0a52170277bb1a64ab4216def3a41
ArianGhsm <arianghsm721@gmail.com>
2026-09-09 00:07:07 +0330

    Install Term 7 group management in Bale runtime

--- bot_runtime/dent_bot/bale_service.py
+++ bot_runtime/dent_bot/bale_service.py
@@ -4,12 +4,14 @@ from .api import BaleBotApi
 from .bale_config import load_settings
 from .class_operations import install_class_operations_product
 from .runtime import run_service
+from .term7_group_management import install_term7_group_management
 
 
 def main() -> int:
     settings = load_settings()
     api = BaleBotApi(settings.token)
     install_class_operations_product()
+    install_term7_group_management()
     return run_service(settings=settings, api=api, platform_name="Bale")
 
 
