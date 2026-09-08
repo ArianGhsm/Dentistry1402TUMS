e579f14bd2cfff8ca6a7173cf16f3f33bca1cfdb
ArianGhsm <arianghsm721@gmail.com>
2026-09-08 14:22:54 +0330

    Mount class operations in Bale product UI

--- bot_runtime/dent_bot/bale_service.py
+++ bot_runtime/dent_bot/bale_service.py
@@ -2,14 +2,14 @@ from __future__ import annotations
 
 from .api import BaleBotApi
 from .bale_config import load_settings
-from .classops_runtime import install_classops_runtime
+from .class_operations_product import install_class_operations_product
 from .runtime import run_service
 
 
 def main() -> int:
     settings = load_settings()
     api = BaleBotApi(settings.token)
-    install_classops_runtime()
+    install_class_operations_product()
     return run_service(settings=settings, api=api, platform_name="Bale")
 
 
