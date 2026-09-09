e5b896f68910392c92c871f5374bded53178dbc6
ArianGhsm <arianghsm721@gmail.com>
2026-09-09 11:41:08 +0330

    feat(bot): install shared home and classops ux v2

--- bot_runtime/dent_bot/service.py
+++ bot_runtime/dent_bot/service.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from .api import TelegramBotApi
+from .bot_home_classops_ux_v2 import install_bot_home_classops_ux_v2
 from .class_operations import install_class_operations_product
 from .config import load_settings
 from .runtime import run_service
@@ -12,6 +13,7 @@ def main() -> int:
     api = TelegramBotApi(settings.token, proxy_url=settings.telegram_proxy_url)
     install_class_operations_product()
     install_term7_group_management()
+    install_bot_home_classops_ux_v2()
     return run_service(settings=settings, api=api, platform_name="Telegram")
 
 
