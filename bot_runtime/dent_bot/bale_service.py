d7f4e68b4f878acdae806cd9a67e1bc27c7edb40
ArianGhsm <arianghsm721@gmail.com>
2026-09-09 11:41:15 +0330

    feat(bot): install shared home and classops ux v2 on bale

--- bot_runtime/dent_bot/bale_service.py
+++ bot_runtime/dent_bot/bale_service.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from .api import BaleBotApi
 from .bale_config import load_settings
+from .bot_home_classops_ux_v2 import install_bot_home_classops_ux_v2
 from .class_operations import install_class_operations_product
 from .runtime import run_service
 from .term7_group_management import install_term7_group_management
@@ -12,6 +13,7 @@ def main() -> int:
     api = BaleBotApi(settings.token)
     install_class_operations_product()
     install_term7_group_management()
+    install_bot_home_classops_ux_v2()
     return run_service(settings=settings, api=api, platform_name="Bale")
 
 
